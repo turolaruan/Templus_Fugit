bc19057b96a48110e12a4f20a48ff8c5631c850f
turolruan <turolaruan2@gmail.com>
2025-05-02 13:21:34 -0300

    update animações e cenas

--- project/Templus Fugit/Assets/Scripts/GameManager.cs
+++ project/Templus Fugit/Assets/Scripts/GameManager.cs
@@ -14,6 +14,9 @@ public class GameManager : MonoBehaviour
     // Dicionário para armazenar a última posição do jogador em cada cena
     private Dictionary<string, Vector3?> savedPositions = new Dictionary<string, Vector3?>();
 
+    // Variável para armazenar a cena anterior
+    private string previousScene = null;
+
     void Awake()
     {
         // Implement the singleton pattern
@@ -43,8 +46,20 @@ public class GameManager : MonoBehaviour
     // Chamado quando uma nova cena é carregada
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        thePlayer = GameObject.FindGameObjectWithTag("Player"); // Atualiza a referência ao jogador
-        PositionPlayerOnSceneLoad(); // Posiciona o jogador na cena carregada
+        // Atualiza a referência ao jogador
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+
+        // Verifica se há uma cena anterior registrada
+        if (!string.IsNullOrEmpty(previousScene))
+        {
+            Debug.Log($"Transição de cena: {previousScene} -> {scene.name}");
+        }
+
+        // Posiciona o jogador na cena carregada
+        PositionPlayerOnSceneLoad();
+
+        // Atualiza a variável da cena anterior após a lógica de transição
+        previousScene = scene.name;
     }
 
     // Start is called before the first frame update
@@ -111,20 +126,46 @@ public class GameManager : MonoBehaviour
     private void PositionPlayerOnSceneLoad()
     {
         string currentScene = SceneManager.GetActiveScene().name;
+        string previousScene = this.previousScene;
+        Debug.Log($"Cena atual: {currentScene}, Cena anterior: {previousScene}");
 
         if (savedPositions.ContainsKey(currentScene) && savedPositions[currentScene].HasValue)
         {
-            if (currentScene == "Cena2")
+            if (currentScene == "Cena2" && previousScene == "Cena3") // transicao da Cena3 para Cena2
             {
-                thePlayer.transform.position = savedPositions[currentScene].Value + new Vector3(0.05f, -0.05f, 0f);
+                thePlayer.transform.position = new Vector3(-6.042691f, 8.385761f, 0f);
             }
-            else if (currentScene == "Cena3")
+            else if (currentScene == "Cena3" && previousScene == "Cena2") // transicao da Cena2 para Cena3
             {
-                thePlayer.transform.position = savedPositions[currentScene].Value + new Vector3(-0.1f, 0f, 0f);
+                thePlayer.transform.position = new Vector3(6.16f, -1.6f, 0f);
             }
-            else if (currentScene == "Cena6")
+            else if (currentScene == "Cena2" && previousScene == "Cena4") // transicao da Cena4 para Cena2
             {
-                thePlayer.transform.position = savedPositions[currentScene].Value + new Vector3(0f, 0.5f, 0f);
+                thePlayer.transform.position = new Vector3(5.78788f, 8.521598f, 0f);
+            }
+            else if (currentScene == "Cena4" && previousScene == "Cena2") // transicao da Cena2 para Cena4
+            {
+                thePlayer.transform.position = new Vector3(-6.16f, -1.03f, 0f);
+            }
+            else if (currentScene == "Cena2" && previousScene == "Cena6") // transicao da Cena6 para Cena2
+            {
+                thePlayer.transform.position = new Vector3(-0.08580431f, 14.966f, 0f);
+            }
+            else if (currentScene == "Cena6" && previousScene == "Cena2") // transicao da Cena2 para Cena6
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.839817f, 0f);
+            }
+            else if (currentScene == "Cena6" && previousScene == "Cena8") 
+            {
+                thePlayer.transform.position = new Vector3(0.07093602f, 1.779929f, 0f);
+            }
+            else if (currentScene == "Cena6" && previousScene == "Cena2")
+            {
+                thePlayer.transform.position = new Vector3(-0.02309176f, -2.92162f, 0f);
+            }
+            else if (currentScene == "Cena8" && previousScene == "Cena6")
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.822216f, 0f);
             }
             else
             {
@@ -172,14 +213,14 @@ public class GameManager : MonoBehaviour
             if (playerPosition.x >= -0.9350259f && playerPosition.x <= 0.935003f && Mathf.Approximately(playerPosition.y, 15.4524f))
             {
                 SavePlayerPosition();
-                SceneManager.LoadScene("Cena6");
+                SceneManager.LoadScene("Cena5");
             }
-            else if (Mathf.Approximately(playerPosition.x, -6.608945f) && playerPosition.y >= 8.068845f && playerPosition.y <= 8.935001f)
+            else if (Mathf.Approximately(playerPosition.x, -6.335011f) && playerPosition.y >= 8.068845f && playerPosition.y <= 8.935001f)
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena3");
             }
-            else if (Mathf.Approximately(playerPosition.x, 6.41f) && playerPosition.y >= 8.064985f && playerPosition.y <= 8.935001f)
+            else if (Mathf.Approximately(playerPosition.x, 6.308114f) && playerPosition.y >= 8.064985f && playerPosition.y <= 8.935001f)
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena4");
@@ -188,7 +229,7 @@ public class GameManager : MonoBehaviour
 
         else if (SceneManager.GetActiveScene().name == "Cena3")
         {
-            if (Mathf.Approximately(playerPosition.x, 6.48f) && playerPosition.y >= -1.935015f && playerPosition.y <= -1.208376f)
+            if (Mathf.Approximately(playerPosition.x, 6.652393f) && playerPosition.y >= -1.935015f && playerPosition.y <= -1.208376f)
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena2");
@@ -197,21 +238,21 @@ public class GameManager : MonoBehaviour
 
         else if (SceneManager.GetActiveScene().name == "Cena4")
         {
-            if (Mathf.Approximately(playerPosition.x, -6.6f) && playerPosition.y >= -1.477549f && playerPosition.y <= -0.5053926f)
+            if (Mathf.Approximately(playerPosition.x, -6.752596f) && playerPosition.y >= -1.477549f && playerPosition.y <= -0.5053926f)
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena2");
             }
         }
 
-        else if (SceneManager.GetActiveScene().name == "Cena6")
+        else if (SceneManager.GetActiveScene().name == "Cena5")
         {
-            if (playerPosition.x >= -0.7060629f && playerPosition.x <= 0.7072755f && Mathf.Approximately(playerPosition.y, -3.354688f))
+            if (playerPosition.x >= -0.7060629f && playerPosition.x <= 0.7072755f && Mathf.Approximately(playerPosition.y, -3.190501f))
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena2");
             }
-            else if (playerPosition.x >= -0.7181748f && playerPosition.x <= 0.7148368f && Mathf.Approximately(playerPosition.y, 2.223252f))
+            else if (playerPosition.x >= -0.7181748f && playerPosition.x <= 0.7148368f && Mathf.Approximately(playerPosition.y, 2.134892f))
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena8");
@@ -225,10 +266,10 @@ public class GameManager : MonoBehaviour
 
         else if (SceneManager.GetActiveScene().name == "Cena8")
         {
-            if (playerPosition.x >= -0.7060629f && playerPosition.x <= 0.7072755f && Mathf.Approximately(playerPosition.y, -3.354688f))
+            if (playerPosition.x >= -0.7060629f && playerPosition.x <= 0.7072755f && Mathf.Approximately(playerPosition.y, -3.107711f))
             {
                 SavePlayerPosition();
-                SceneManager.LoadScene("Cena6");
+                SceneManager.LoadScene("Cena5");
             }
         }
 
@@ -237,7 +278,7 @@ public class GameManager : MonoBehaviour
             if (Mathf.Approximately(playerPosition.x, -6.678032f) && playerPosition.y >= -1.475696f && playerPosition.y <= 0.5104183f)
             {
                 SavePlayerPosition();
-                SceneManager.LoadScene("Cena6");
+                SceneManager.LoadScene("Cena5");
             }
         }
     }
