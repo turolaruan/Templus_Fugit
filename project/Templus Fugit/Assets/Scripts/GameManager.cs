2af78807950d0048888593499b7b032cac225a3f
turolruan <turolaruan2@gmail.com>
2025-05-02 18:12:37 -0300

    update mapa quase completo

--- project/Templus Fugit/Assets/Scripts/GameManager.cs
+++ project/Templus Fugit/Assets/Scripts/GameManager.cs
@@ -147,25 +147,97 @@ public class GameManager : MonoBehaviour
             {
                 thePlayer.transform.position = new Vector3(-6.16f, -1.03f, 0f);
             }
-            else if (currentScene == "Cena2" && previousScene == "Cena6") // transicao da Cena6 para Cena2
+            else if (currentScene == "Cena2" && previousScene == "Cena5") // transicao da Cena5 para Cena2
             {
                 thePlayer.transform.position = new Vector3(-0.08580431f, 14.966f, 0f);
             }
-            else if (currentScene == "Cena6" && previousScene == "Cena2") // transicao da Cena2 para Cena6
+            else if (currentScene == "Cena5" && previousScene == "Cena2") // transicao da Cena2 para Cena5
             {
-                thePlayer.transform.position = new Vector3(0.02f, -2.839817f, 0f);
+                thePlayer.transform.position = new Vector3(0.02f, -2.710258f, 0f);
             }
-            else if (currentScene == "Cena6" && previousScene == "Cena8") 
+            else if (currentScene == "Cena5" && previousScene == "Cena10") // transicao da Cena10 para Cena5
             {
-                thePlayer.transform.position = new Vector3(0.07093602f, 1.779929f, 0f);
+                thePlayer.transform.position = new Vector3(-6.265987f, -0.5999068f, 0f);
             }
-            else if (currentScene == "Cena6" && previousScene == "Cena2")
+            else if (currentScene == "Cena10" && previousScene == "Cena5") // transicao da Cena5 para Cena10
             {
-                thePlayer.transform.position = new Vector3(-0.02309176f, -2.92162f, 0f);
+                thePlayer.transform.position = new Vector3(6.02f, -0.66f, 0f);
             }
-            else if (currentScene == "Cena8" && previousScene == "Cena6")
+            else if (currentScene == "Cena5" && previousScene == "Cena9") // transicao da Cena9 para Cena5
             {
-                thePlayer.transform.position = new Vector3(0.02f, -2.822216f, 0f);
+                thePlayer.transform.position = new Vector3(6.159083f, -0.532959f, 0f);
+            }
+            else if (currentScene == "Cena9" && previousScene == "Cena5") // transicao da Cena5 para Cena9
+            {
+                thePlayer.transform.position = new Vector3(-6.272023f, -0.66f, 0f);
+            }
+            else if (currentScene == "Cena5" && previousScene == "Cena6") // transicao da Cena6 para Cena5
+            {
+                thePlayer.transform.position = new Vector3(0.02f, 1.730677f, 0f);
+            }
+            else if (currentScene == "Cena6" && previousScene == "Cena5") // transicao da Cena5 para Cena6
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.7f, 0f);
+            }
+            else if (currentScene == "Cena6" && previousScene == "Cena15") // transicao da Cena15 para Cena6
+            {
+                thePlayer.transform.position = new Vector3(0.02f, 1.730677f, 0f);
+            }
+            else if (currentScene == "Cena15" && previousScene == "Cena6") // transicao da Cena6 para Cena15
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.7f, 0f);
+            }
+            else if (currentScene == "Cena10" && previousScene == "Cena11") // transicao da Cena11 para Cena10
+            {
+                thePlayer.transform.position = new Vector3(0.02f, 1.730677f, 0f);
+            }
+            else if (currentScene == "Cena11" && previousScene == "Cena10") // transicao da Cena10 para Cena11
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.7f, 0f);
+            }
+            else if (currentScene == "Cena9" && previousScene == "Cena12") // transicao da Cena12 para Cena9
+            {
+                thePlayer.transform.position = new Vector3(0.02f, 1.730677f, 0f);
+            }
+            else if (currentScene == "Cena12" && previousScene == "Cena9") // transicao da Cena9 para Cena12
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.7f, 0f);
+            }
+            else if (currentScene == "Cena11" && previousScene == "Cena13") // transicao da Cena13 para Cena11
+            {
+                thePlayer.transform.position = new Vector3(0.02f, 1.730677f, 0f);
+            }
+            else if (currentScene == "Cena13" && previousScene == "Cena11") // transicao da Cena11 para Cena13
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.7f, 0f);
+            }
+            else if (currentScene == "Cena12" && previousScene == "Cena14") // transicao da Cena14 para Cena12
+            {
+                thePlayer.transform.position = new Vector3(0.02f, 1.730677f, 0f);
+            }
+            else if (currentScene == "Cena14" && previousScene == "Cena12") // transicao da Cena12 para Cena14
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.7f, 0f);
+            }
+            else if (currentScene == "Cena13" && previousScene == "Cena15") // transicao da Cena15 para Cena13
+            {
+                thePlayer.transform.position = new Vector3(6.105287f, -0.4260389f, 0f);
+            }
+            else if (currentScene == "Cena15" && previousScene == "Cena13") // transicao da Cena13 para Cena15
+            {
+                thePlayer.transform.position = new Vector3(-5.948434f, -0.519862f, 0f);
+            }
+            else if (currentScene == "Cena15" && previousScene == "Cena14") // transicao da Cena14 para Cena15
+            {
+                thePlayer.transform.position = new Vector3(5.942899f, -0.4849414f, 0f);
+            }
+            else if (currentScene == "Cena14" && previousScene == "Cena15") // transicao da Cena15 para Cena14
+            {
+                thePlayer.transform.position = new Vector3(-6.119143f, -0.6036257f, 0f);
+            }
+            else if (currentScene == "Cena16" && previousScene == "Cena15") // transicao da Cena15 para Cena16
+            {
+                thePlayer.transform.position = new Vector3(0.02f, -2.7f, 0f);
             }
             else
             {
@@ -247,20 +319,39 @@ public class GameManager : MonoBehaviour
 
         else if (SceneManager.GetActiveScene().name == "Cena5")
         {
-            if (playerPosition.x >= -0.7060629f && playerPosition.x <= 0.7072755f && Mathf.Approximately(playerPosition.y, -3.190501f))
+            if (playerPosition.x >= -0.7820935f && playerPosition.x <= 0.8307027f && Mathf.Approximately(playerPosition.y, -3.100172f))
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena2");
             }
-            else if (playerPosition.x >= -0.7181748f && playerPosition.x <= 0.7148368f && Mathf.Approximately(playerPosition.y, 2.134892f))
+            else if (playerPosition.x >= -0.7181748f && playerPosition.x <= 0.7148368f && Mathf.Approximately(playerPosition.y, 2.066446f))
             {
                 SavePlayerPosition();
-                SceneManager.LoadScene("Cena8");
+                SceneManager.LoadScene("Cena6");
             }
             else if (Mathf.Approximately(playerPosition.x, 6.514736f) && playerPosition.y >= -1.51467f && playerPosition.y <= 0.5432134f)
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena9");
+            }
+            else if (Mathf.Approximately(playerPosition.x, -6.772685f) && playerPosition.y >= -1.605973f && playerPosition.y <= 0.5947051f)
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena10");
+            }
+        }
+
+        else if (SceneManager.GetActiveScene().name == "Cena6")
+        {
+            if (playerPosition.x >= -0.7820935f && playerPosition.x <= 0.8307027f && Mathf.Approximately(playerPosition.y, -3.11787f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena5");
+            }
+            else if (playerPosition.x >= -0.7684372f && playerPosition.x <= 0.760188f && Mathf.Approximately(playerPosition.y, 2.016829f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena15");
             }
         }
 
@@ -275,10 +366,103 @@ public class GameManager : MonoBehaviour
 
         else if (SceneManager.GetActiveScene().name == "Cena9")
         {
-            if (Mathf.Approximately(playerPosition.x, -6.678032f) && playerPosition.y >= -1.475696f && playerPosition.y <= 0.5104183f)
+            if (Mathf.Approximately(playerPosition.x, -6.678032f) && playerPosition.y >= -1.55167f && playerPosition.y <= 0.5063582f)
             {
                 SavePlayerPosition();
                 SceneManager.LoadScene("Cena5");
+            }
+            else if (playerPosition.x >= -0.7181748f && playerPosition.x <= 0.7148368f && Mathf.Approximately(playerPosition.y, 2.003242f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena12");
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "Cena10")
+        {
+            if (Mathf.Approximately(playerPosition.x, 6.734772f) && playerPosition.y >= -1.521577f && playerPosition.y <= 0.5304125f)
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena5");
+            }
+            else if (playerPosition.x >= -0.7181748f && playerPosition.x <= 0.7148368f && Mathf.Approximately(playerPosition.y, 2.003242f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena11");
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "Cena11")
+        {
+            if (playerPosition.x >= -0.7820935f && playerPosition.x <= 0.8307027f && Mathf.Approximately(playerPosition.y, -3.11787f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena10");
+            }
+            else if (playerPosition.x >= -0.7181748f && playerPosition.x <= 0.7148368f && Mathf.Approximately(playerPosition.y, 2.003242f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena13");
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "Cena12")
+        {
+            if (playerPosition.x >= -0.7820935f && playerPosition.x <= 0.8307027f && Mathf.Approximately(playerPosition.y, -3.11787f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena9");
+            }
+            else if (playerPosition.x >= -0.7181748f && playerPosition.x <= 0.7148368f && Mathf.Approximately(playerPosition.y, 2.003242f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena14");
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "Cena13")
+        {
+            if (playerPosition.x >= -0.7820935f && playerPosition.x <= 0.8307027f && Mathf.Approximately(playerPosition.y, -3.11787f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena11");
+            }
+            else if (Mathf.Approximately(playerPosition.x, 6.600544f) && playerPosition.y >= -1.586784f && playerPosition.y <= 0.5661189f)
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena15");
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "Cena14")
+        {
+            if (playerPosition.x >= -0.7820935f && playerPosition.x <= 0.8307027f && Mathf.Approximately(playerPosition.y, -3.11787f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena12");
+            }
+            else if (Mathf.Approximately(playerPosition.x, -6.746841f) && playerPosition.y >= -1.55167f && playerPosition.y <= 0.5063582f)
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena15");
+            }
+        }
+        else if (SceneManager.GetActiveScene().name == "Cena15")
+        {
+            if (playerPosition.x >= -0.7820935f && playerPosition.x <= 0.8307027f && Mathf.Approximately(playerPosition.y, -3.100172f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena6");
+            }
+            else if (Mathf.Approximately(playerPosition.x, -6.678032f) && playerPosition.y >= -1.55167f && playerPosition.y <= 0.5063582f)
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena13");
+            }
+            else if (Mathf.Approximately(playerPosition.x, 6.600544f) && playerPosition.y >= -1.568303f && playerPosition.y <= 0.5683065f)
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena14");
+            }
+            else if (playerPosition.x >= -0.7296513f && playerPosition.x <= 0.7507334f && Mathf.Approximately(playerPosition.y, 2.066446f))
+            {
+                SavePlayerPosition();
+                SceneManager.LoadScene("Cena16_Boss");
             }
         }
     }
