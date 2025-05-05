36c9bc9426f2b9d3f9a4ab94952e5beb66c0cd8f
Adriel Foppa <adrielfoppa@gmail.com>
2025-05-05 16:23:44 -0300

    Identifica se um item esta na cena de loja, caso nao esteja, o preco é removido

--- project/Templus Fugit/Assets/Scripts/StoreItem.cs
+++ project/Templus Fugit/Assets/Scripts/StoreItem.cs
@@ -2,10 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class StoreItem : MonoBehaviour
 {
     public int preco;
+
+    private void Start()
+    {
+        // Verifica se a cena atual é "Store"
+        if (SceneManager.GetActiveScene().name != "Store")
+        {
+            // Tenta encontrar o filho chamado "PrecoTexto" e desativá-lo
+            Transform precoTexto = transform.Find("PrecoTexto");
+            if (precoTexto != null)
+            {
+                precoTexto.gameObject.SetActive(false);
+            }
+        }
+    }
 
     public void DefinirPreco(int valor)
     {
